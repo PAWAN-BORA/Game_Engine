be20c2d06ac2cd17b4668ff3cc7f5bf5140222c2
pawan <funwithcodings@gmail.com>
2019-12-13 18:25:14 +0530

    creating window

--- src/com/base/engine/MainComponent.java
+++ src/com/base/engine/MainComponent.java
@@ -1,8 +1,38 @@
 package com.base.engine;
 
+import org.lwjgl.glfw.GLFW;
+
 public class MainComponent {
     public static void main(String[] args) {
         System.out.println("num");
+        Window.createWindow(500, 500);
+        MainComponent game = new MainComponent();
+        game.start();
+
     }
+    public void mainComponent() {
+
+    }
+    public void start() {
+        run();
+    }
+    public void stop() {
+
+    }
+    public void run() {
+        while (!Window.isClose()) {
+            System.out.println("running");
+            Window.update();
+            Window.swapBuffer();
+            render();
+        }
+    }
+    public void render() {
+
+    }
+    public void cleanUp() {
+
+    }
+
 
 }
--- src/com/base/engine/Window.java
+++ src/com/base/engine/Window.java
@@ -1,8 +1,34 @@
 package com.base.engine;
 
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWVidMode;
+
 public class Window {
+    private static  long window;
     public static void createWindow(int width, int height){
-
+        if(!GLFW.glfwInit()) {
+            System.err.println("could not initialize GLFW");
+        }
+        Window.window = GLFW.glfwCreateWindow(width, height, "Title", 0, 0);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_FALSE);
+        if(Window.window==0) {
+            System.err.println("Could not initialize the window");
+            System.exit(-1);
+        }
+        GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+        assert videoMode != null;
+        GLFW.glfwSetWindowPos(Window.window, (videoMode.width()-width)/2, (videoMode.height()-height)/2);
+        GLFW.glfwShowWindow(Window.window);
     }
-
+    public static boolean isClose() {
+        return  GLFW.glfwWindowShouldClose(Window.window);
+    }
+    public static void update() {
+        GLFW.glfwPollEvents();
+    }
+    public static  void swapBuffer() {
+        GLFW.glfwSwapBuffers(Window.window);
+    }
 }
